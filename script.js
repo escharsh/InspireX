dbbcbb5325609b56f447d30feca7fc0c9ef64f7e
Harshvardhan <148743679+yuthorius@users.noreply.github.com>
2024-11-20 20:32:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const facts = [
-    "I love hiking and have traveled to five national parks.",
-    "I am an avid reader and read around 20 books per year.",
-    "I once baked a 3-tiered cake for a family gathering!",
-    "I speak three languages fluently.",
-    "I’m learning to play the guitar in my free time."
+    " Hi my name is Harshvardhan.",
+    " I am 22 years old",
+    " I love to play video games sometime code and sometimes singing!",
+    "I speak Hindi, English, Punjab languages fluently.",
+    "I’m learning to Full stack development in my free time."
 ];
 
 document.getElementById('generate-fact').addEventListener('click', function() {
